372f78724c2d7f0ef293cf91af5bf39ae20fd7d3
Matias <mgaldame@fen.uchile.cl>
2023-03-30 02:06:22 -0300

    Update rename_materials.py
    The option to rename is added if this name already exists.

--- rename_materials.py
+++ rename_materials.py
@@ -1,8 +1,17 @@
 import bpy
 
 def rename_materials():
+    # Recorremos todos los materiales de la escena
     for material in bpy.data.materials:
-        if material.name:
-            new_name = material.name.lower().replace(" ", "_")
-            new_name = "".join(c if c.isalnum() else "_" for c in new_name)
-            material.name = new_name
+        # Si el material ya tiene el nombre deseado, pasamos al siguiente
+        if material.name.islower() and "_" not in material.name and material.name.isalnum():
+            continue
+        # Creamos un nuevo nombre para el material
+        new_name = ''.join(c if c.isalnum() else "_" for c in material.name.lower())
+        # Si el nombre ya existe, agregamos un número al final
+        i = 0
+        while new_name in bpy.data.materials and material.name != new_name:
+            i += 1
+            new_name = ''.join(c if c.isalnum() else "_" for c in material.name.lower()) + f"_{i}"
+        # Renombramos el material
+        material.name = new_name
